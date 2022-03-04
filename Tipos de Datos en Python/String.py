e97cdbd58ee96bbf7e8f6bc10035765e8405a783
Petter10C <99735654+Petter10C@users.noreply.github.com>
2022-03-03 19:35:13 -0500

    Update String.py

--- Tipos de Datos en Python/String.py
+++ Tipos de Datos en Python/String.py
@@ -1,1 +1,8 @@
+#Ejecucion de String
 
+cad = input("Introduce una cadena:")
+while True:
+    car = input("Introduce un carácter:")
+    if len(car) == 1: break
+
+print("En la cadena",cad,"aparecen",cad.count(car),"veces el carácter",car)
