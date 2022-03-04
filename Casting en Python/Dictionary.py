a8ba5193455c50d5b282193694b6e77a562b3492
Petter10C <99735654+Petter10C@users.noreply.github.com>
2022-03-03 19:36:30 -0500

    Update Dictionary.py

--- Casting en Python/Dictionary.py
+++ Casting en Python/Dictionary.py
@@ -1,11 +1,9 @@
 #Ejecucion de Dictionary
 
-my_diccionario1 = {'nombre': 'mike', 'edad': 18}
-
-    print(my_diccionario1.get['nombre'])
-    
-    print(my_diccionario1.get('edad'))
-    
-    print(my_diccionario1.get('dirección'))
-    
-    print(my_diccionario1.get['dirreción'])
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print("............")
